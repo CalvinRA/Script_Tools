3c6be16fd7d8ef8665026ba9ae8db5911ef9e2dc
CalvinRA <97717845+CalvinRA@users.noreply.github.com>
2024-01-23 20:39:54 -0700

    Added Scripts from Past Projects

---
+++ Past Project Scripts/AnimStatesBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimStatesBehaviour : StateMachineBehaviour
+{
+    public UnityEvent onEnterEvent;
+    public UnityEvent onUpdateEvent;
+    public UnityEvent onExitEvent;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Custom logic here
+        onEnterEvent.Invoke();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Custom logic here
+        onUpdateEvent.Invoke();
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Custom logic here
+        onExitEvent.Invoke();
+    }
+}
---
+++ Past Project Scripts/GameActionHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Grabbed from Anothony Romrell's git repository. - thanks professor!
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction action;
+    public UnityEvent startEvent, respondEvent, respondLateEvent, runInAnimatorEvent;
+    public float holdTime = 0.1f;
+    private WaitForSeconds waitObj;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        waitObj = new WaitForSeconds(holdTime);
+        action.raiseNoArgs += Respond;
+    }
+
+    private void Respond()
+    {
+        respondEvent.Invoke();
+        StartCoroutine(RespondLate());
+    }
+
+    private IEnumerator RespondLate()
+    {
+        yield return waitObj;
+        respondLateEvent.Invoke();
+    }
+
+    private void RunFromAnimator()
+    {
+        runInAnimatorEvent.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        action.raiseNoArgs = null;
+    }
+}
